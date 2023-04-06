269d1820684af1f59156767844756503bbd7d2b3
yjjem <marksim0606@gmail.com>
2023-04-06 16:57:35 +0900

    Initial Project setup

---
+++ magic-transition/magic-transition/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  magic-transition
+//
+//  Copyright (c) 2023 Jeremy All rights reserved.
+    
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
