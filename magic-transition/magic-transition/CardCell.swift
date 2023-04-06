a7aae489fb3b158124892089e2fa28d01fa9567b
yjjem <marksim0606@gmail.com>
2023-04-06 17:40:47 +0900

    feat: tableview, CardCell

--- magic-transition/magic-transition/CardCell.swift
+++ magic-transition/magic-transition/CardCell.swift
@@ -5,4 +5,64 @@
 //  Copyright (c) 2023 Jeremy All rights reserved.
     
 
-import Foundation
+import UIKit
+
+final class CardCell: UITableViewCell {
+    
+    let largeImageView: UIView = .init()
+    let titleLabel: UILabel = .init()
+    let secondaryTitleLabel: UILabel = .init()
+    let descriptionLabel: UILabel = .init()
+    let contentStack: UIStackView = .init()
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        titleLabel.text = nil
+        secondaryTitleLabel.text = nil
+        descriptionLabel.text = nil
+
+    }
+    
+    func configureTextDesign() {
+        titleLabel.font = .systemFont(ofSize: 36, weight: .bold)
+        secondaryTitleLabel.font = .systemFont(ofSize: 24, weight: .regular)
+        secondaryTitleLabel.textColor = .systemGray2
+    }
+    
+    func configureLayout() {
+        configureTextDesign()
+        largeImageView.backgroundColor = .systemBlue
+        
+        contentStack.translatesAutoresizingMaskIntoConstraints = false
+        contentStack.addArrangedSubview(largeImageView)
+        contentStack.addArrangedSubview(secondaryTitleLabel)
+        contentStack.addArrangedSubview(titleLabel)
+        contentStack.addArrangedSubview(descriptionLabel)
+        
+        contentStack.axis = .vertical
+        contentStack.alignment = .center
+        contentStack.distribution = .equalCentering
+        
+        contentView.addSubview(contentStack)
+        
+        NSLayoutConstraint.activate([
+            contentStack.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 30),
+            contentStack.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            contentStack.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            contentStack.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -30),
+            contentStack.heightAnchor.constraint(equalToConstant: 400)
+        ])
+        
+        NSLayoutConstraint.activate([
+            largeImageView.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.8),
+            largeImageView.heightAnchor.constraint(equalTo: largeImageView.widthAnchor)
+        ])
+    }
+    
+    func insertData(title: String, secondary: String, description: String) {
+        titleLabel.text = title
+        secondaryTitleLabel.text = secondary
+        descriptionLabel.text = description
+    }
+}
--- magic-transition/magic-transition/ViewController.swift
+++ magic-transition/magic-transition/ViewController.swift
@@ -7,13 +7,65 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+    
+    let tableView = UITableView(frame: .init(), style: .insetGrouped)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        configureViews()
     }
-
-
+    
+    func configureViews() {
+        view.addSubview(tableView)
+        view.backgroundColor = .white
+        
+        tableView.register(CardCell.self, forCellReuseIdentifier: "cell")
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.backgroundColor = .systemGray4
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
 }
 
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 18, weight: .semibold)
+        label.text = "Section \(section)"
+        
+        return label
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? CardCell else { return CardCell() }
+        cell.configureLayout()
+        cell.insertData(title: "Title", secondary: "Secondary", description: "Description")
+        return cell
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
